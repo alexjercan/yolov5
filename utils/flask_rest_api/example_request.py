4c9a4b22b09b0e03bb6e4ca3abd8a0f9af926cdf
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-04-30 21:56:47 +0300

    idk

--- utils/flask_rest_api/example_request.py
+++ utils/flask_rest_api/example_request.py
@@ -1,12 +1,69 @@
 """Perform test request"""
+import os
+import io
+import cv2
+import pickle
+import numpy as np
+
 import pprint
 
 import requests
 
-DETECTION_URL = "http://localhost:5000/v1/object-detection/yolov5s"
-TEST_IMAGE = "zidane.jpg"
 
-image_data = open(TEST_IMAGE, "rb").read()
+def png2bgr(path):
+    if not os.path.isfile(path):
+            return None
+
+    img = cv2.imread(path)
+    img = np.array(img).astype(np.float32).reshape(img.shape[0], img.shape[1], -1)
+    img = img / 255
+    
+    return img
+
+
+def exr2depth(path):
+    if not os.path.isfile(path):
+            return None
+        
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE | cv2.IMREAD_ANYDEPTH)
+
+    maxvalue = np.max(img[img < np.max(img)])
+    img[img > maxvalue] = maxvalue
+    img = img / maxvalue
+
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    img = np.array(img).astype(np.float32).reshape(img.shape[0], img.shape[1], -1)
+
+    return img
+
+
+def exr2normal(path):
+    if not os.path.isfile(path):
+            return None
+
+    img = cv2.imread(path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+    img = np.array(img).astype(np.float32).reshape(img.shape[0], img.shape[1], -1)
+
+    return img
+
+def save_layers(path, data: np.ndarray):
+    cv2.imwrite(path, data, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+
+DETECTION_URL = "http://localhost:5000/v1/object-detection/yolov5s"
+TEST_RGB = "000000rgb.png"
+TEST_DEPTH = "000000depth.exr"
+TEST_NORMAL = "000000normal.exr"
+
+image_data = png2bgr(TEST_RGB)
+depth_data = exr2depth(TEST_DEPTH)
+normal_data = exr2normal(TEST_NORMAL)
+
+layers = np.concatenate([image_data, depth_data, normal_data], axis=2)
+
+bytes_io = io.BytesIO()
+pickle.dump(layers, bytes_io)
+bytes_io.seek(0)
+image_data = bytes_io.read()
 
 response = requests.post(DETECTION_URL, files={"image": image_data}).json()
 
--- utils/flask_rest_api/restapi.py
+++ utils/flask_rest_api/restapi.py
@@ -1,11 +1,11 @@
 """
 Run a rest API exposing the yolov5s object detection model
 """
-import argparse
 import io
+import pickle
+import argparse
 
 import torch
-from PIL import Image
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -19,12 +19,11 @@ def predict():
         return
 
     if request.files.get("image"):
-        image_file = request.files["image"]
-        image_bytes = image_file.read()
-
-        img = Image.open(io.BytesIO(image_bytes))
-
-        results = model(img, size=640)  # reduce size=320 for faster inference
+        image_bytes = request.files["image"]
+        image_bytes = io.BytesIO(image_bytes.read())
+        img = pickle.load(image_bytes)
+       
+        results = model(img, size=256, conf=0.01, iou=0.01)
         return results.pandas().xyxy[0].to_json(orient="records")
 
 
@@ -32,6 +31,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask API exposing YOLOv5 model")
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
-
-    model = torch.hub.load("ultralytics/yolov5", "yolov5s", force_reload=True)  # force_reload to recache
+    model = torch.hub.load("alexjercan/yolov5", "yolov5s", force_reload=True)  # force_reload to recache
+    model.eval()
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
